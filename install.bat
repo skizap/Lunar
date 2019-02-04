6ff8847319e3ee4fd258ecdf5ea26d30888a9e8b
3 <36322593+MetaChar@users.noreply.github.com>
2019-02-03 20:02:32 -0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,11 @@
+title LUNAR INSTALL
+pip install requests
+pip install datetime
+pip install subprocess
+pip install colorama
+pip install cryptography
+pip install urllib2
+git clone https://github.com/wireshark/wireshark
+git clone https://github.com/vanhauser-thc/thc-hydra
+echo "DONE PRESS [ENTER] TO LEAVE"
+pause
