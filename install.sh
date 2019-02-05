0762fb2924b4a732fc38206ca33f0ba014a713be
3 <36322593+MetaChar@users.noreply.github.com>
2019-02-05 14:32:58 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,14 @@ pip install subprocess
 pip install colorama
 pip install cryptography
 pip install urllib2
-git clone https://github.com/wireshark/wireshark
-git clone https://github.com/vanhauser-thc/thc-hydra
+sudo add-apt-repository ppa:wireshark-dev/stable
+sudo apt-get update
+sudo apt-get install wireshark
+sudo dpkg-reconfigure wireshark-common
+sudo adduser $USER wireshark
+sudo add-apt-repository ppa:pi-rho/security
+sudo apt-get install hydra hydra-gtk
+sudo apt-get update
+sudo apt-get install hydra
 echo "done!"
+echo "May need to resart your pc to use the wireshark option!"
